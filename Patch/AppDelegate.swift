b8d20c2792150d70e904fa59e32ddab077070269
Jacob Budin <self@jacobbudin.com>
2017-03-15 20:37:13 -0400

    Initial Commit

---
+++ Patch/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Patch
+//
+//  Created by Jacob Budin on 3/15/17.
+//  Copyright © 2017 Jacob Budin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
